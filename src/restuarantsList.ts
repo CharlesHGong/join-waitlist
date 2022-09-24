b69663c4b86fd2f8205a292931ec8c84ffa362a6
Charles Gong <hanning.charles.gong@gmail.com>
2022-09-24 15:39:03 -0400

    update list

---
+++ src/restuarantsList.ts
@@ -0,0 +1,46 @@
+export const restuarants = [
+  {
+    sid: "s1223e6df72a8e66b98960990d70833dd",
+    name: "花小姐金汤花胶鸡",
+  },
+  {
+    sid: "scb30539079a1df7a194b1cb77e62f786",
+    name: "麻辣空间",
+  },
+  {
+    sid: "s71b8365e1a1ea0c8f71a144224ac781a",
+    name: "小食代",
+  },
+  {
+    sid: "sf748603765bf63193d4dbf524fa3da82",
+    name: "uluh",
+  },
+  {
+    sid: "se62970b73842d2f4e669a2531e76a3d4",
+    name: "重庆老灶",
+  },
+  {
+    sid: "s6521a4c803bf43f0a069cfc52e514b00",
+    // name: 'Spot'
+  },
+  {
+    sid: "s6534c66ca9144862b2ece14a7ee0f191",
+    name: "浙里",
+  },
+  {
+    sid: "sdafbebc005977eb72a7f67e23d7e621d",
+    name: '穿山甲'
+  },
+  {
+    sid: "scebbff61f0e638fb039a7a2ecc17a7d1",
+    name: "山城码头",
+  },
+  // {
+  //   sid: "s138fb0313d104c245294a39abf998811",
+  //   name: "贰火锅",
+  // },
+  // {
+  //   sid:"s63cdc1be700fb282d14e89c39cb5dfad",
+  //   name:'胡桃里'
+  // }
+];
